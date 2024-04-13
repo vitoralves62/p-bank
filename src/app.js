8a409ed59e1e84cfcc78443106e0019b45be3bf0
Henrique de Azara Tosta <henriqueazara3311@gmail.com>
2024-04-13 11:35:40 -0300

    First commit

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 var corsOptions = {
     origin: '*',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200, 
   };
 
 app.use(cors(corsOptions));
